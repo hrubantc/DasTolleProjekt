7be22b8086f8b41c63df95c7e621cbff4721661b
sebnes <41290900+sebnes@users.noreply.github.com>
2018-07-20 12:47:06 +0200

    sebnesCube

---
+++ Assets/Scripts/sebnesCubeMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sebnesCubeMovement : MonoBehaviour {
+
+    float speed = 10f;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+
+        translation *= Time.deltaTime;     
+
+        
+        transform.Translate(0, 0, translation);
+    }
+}
