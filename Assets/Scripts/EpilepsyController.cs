214d66c80e61e77f0a208b15a308ab7163832d4c
DasNanda <Nandix.myroom@gmx.at>
2018-07-20 12:35:45 +0200

    Added EpilepsyController

---
+++ Assets/Scripts/EpilepsyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EpilepsyController : MonoBehaviour
+{
+    public float pauseInterval;
+    public float pauseLength;
+
+    private Material mat;
+    private bool paused = false;
+
+
+    void Start()
+    {
+        mat = GetComponent<MeshRenderer>().material;
+        StartCoroutine(PauseBlink());
+    }
+
+    void FixedUpdate ()
+    {
+        if (!paused)
+            mat.color = Random.ColorHSV();
+	}
+
+    IEnumerator PauseBlink()
+    {
+        yield return new WaitForSeconds(pauseInterval);
+        paused = true;
+        StartCoroutine(ResumeBlink());
+    }
+
+    IEnumerator ResumeBlink()
+    {
+        yield return new WaitForSeconds(pauseLength);
+        paused = false;
+        StartCoroutine(PauseBlink());
+    }
+}
